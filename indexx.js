5fcadf2e36d21fe18c47a8d3c9d33ca64205e45f
VitorLuuvy <102753417+VitorLuuvy@users.noreply.github.com>
2022-12-21 18:17:31 -0300

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+const path = require('path');
+
+server.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+server.get('/perfil', (req, res)=> {
+  res.sendFile(path.join(__dirname+'/perfil.html'));
+})
+
+server.get('/tarefas', (req, res)=> {
+  res.sendFile(path.join(__dirname+'/tarefas.tml'));
+})
+
+server.get('/registrar', (req, res)=> {
+  res.sendFile(path.join(__dirname+'/registrar.html'));
+})
+
+server.get('/login', (req, res)=> {
+  res.sendFile(path.join(__dirname+'/projeto.html'));
+})
+
+
+
+server.listen(3000);
